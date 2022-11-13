68952bf1a19b31123131ad334792cb1236ef7ac9
noncasted <noncasted@gmail.com>
2022-11-13 20:15:24 +0300

    props imported

---
+++ Assets/Features/GamePlay/Level/Environment/Tools/ChunkPropsConfigurator.cs
@@ -0,0 +1,29 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace GamePlay.Level.Environment.Tools
+{
+    public class ChunkPropsConfigurator : MonoBehaviour
+    {
+        [Button("Configure")]
+        private void Configure()
+        {
+            var childs = new GameObject[transform.childCount];
+
+            for (var i = 0; i < childs.Length; i++)
+                childs[i] = transform.GetChild(i).gameObject;
+
+
+            foreach (var child in childs)
+            {
+                if (child.TryGetComponent(out SortingGroup sortingGroup) == true)
+                    continue;
+
+                var group = child.AddComponent<SortingGroup>();
+                group.sortingLayerName = "Location";
+                group.sortingOrder = 20;
+            }
+        }
+    }
+}

92f7abbdbfbe3d633224955144e224043ac1a8f0
noncasted <noncasted@gmail.com>
2022-11-15 02:05:03 +0300

    interpolation added

--- Assets/Features/GamePlay/Player/Entity/Network/Views/Transforms/Runtime/NetworkTransform.cs
+++ Assets/Features/GamePlay/Player/Entity/Network/Views/Transforms/Runtime/NetworkTransform.cs
@@ -1,4 +1,5 @@
-﻿using Global.Services.Network.Instantiators.Runtime;
+﻿using System;
+using Global.Services.Network.Instantiators.Runtime;
 using Ragon.Client;
 using UnityEngine;
 
@@ -7,15 +8,25 @@ namespace GamePlay.Player.Entity.Network.Views.Transforms.Runtime
     [DisallowMultipleComponent]
     public class NetworkTransform : RagonBehaviour, INetworkTransform
     {
-        private readonly RagonVector3 _position = new(Vector3.zero, RagonAxis.XY);
-        private Vector2 _lastReplicated;
+        [SerializeField] private float _interpolationBreakDistance = 1f;
+        [SerializeField] private float _interpolationSpeed = 1f;
 
+        private readonly RagonVector3 _position = new(Vector3.zero, RagonAxis.XY);
+
+        private Vector2 _lastReplicated;
         private Vector2 _localPosition;
+
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
 
         public void SetPosition(Vector2 position)
         {
             _localPosition = position;
-            transform.position = position;
+            _transform.position = position;
         }
 
         public override void OnCreatedEntity()
@@ -31,7 +42,19 @@ namespace GamePlay.Player.Entity.Network.Views.Transforms.Runtime
 
         public override void OnProxyTick()
         {
-            transform.position = _position.Value;
+            var target = _position.Value;
+            var current = _transform.position;
+            
+            var distance = Vector3.Distance(target, current);
+
+            if (distance > _interpolationBreakDistance)
+            {
+                _transform.position = current;
+                return;
+            }
+            
+            _transform.position =
+                Vector3.Lerp(_transform.position, current, Time.deltaTime * _interpolationSpeed);
         }
     }
 }

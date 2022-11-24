5110eed61348343cf6a1fa96cf79e054d85dfc68
noncasted <noncasted@gmail.com>
2022-11-24 21:21:59 +0300

    wip

---
+++ Assets/Features/Global/Services/Sounds/Runtime/SoundsMessageBrokerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UniRx;
+
+namespace Global.Services.Sounds.Runtime
+{
+    public static class SoundsMessageBrokerExtensions
+    {
+        public static void TriggerSound(this MessageBroker messageBroker, SoundType sound)
+        {
+            var data = new SoundEvent(sound);
+            messageBroker.Publish(data);
+        }
+    }
+}

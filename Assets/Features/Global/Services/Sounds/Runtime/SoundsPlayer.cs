8982a986503ed9b0082a20cba471827fdc78aaf0
noncasted <noncasted@gmail.com>
2022-11-26 02:58:20 +0300

    wip

--- Assets/Features/Global/Services/Sounds/Runtime/SoundsPlayer.cs
+++ Assets/Features/Global/Services/Sounds/Runtime/SoundsPlayer.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-
+﻿using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
 
 namespace Global.Services.Sounds.Runtime
 
@@ -7,63 +8,50 @@ namespace Global.Services.Sounds.Runtime
     [DisallowMultipleComponent]
     public class SoundsPlayer : MonoBehaviour
     {
-        [Space(30)]
-        [Header("Shot")]
-        [SerializeField]
-        public FMODUnity.EventReference ShotEvent;
+        [Space(30)] [Header("Shot")] [SerializeField]
+        public EventReference ShotEvent;
         public FMOD.Studio.EventInstance ShotInstance;
 
-        [Space(30)]
-        [Header("Ambience")]
-        [SerializeField]
-        public FMODUnity.EventReference AmbEvent;
+        [Space(30)] [Header("Ambience")] [SerializeField]
+        public EventReference AmbEvent;
         public FMOD.Studio.EventInstance AmbInstance;
 
-        [Space(30)]
-        [Header("Music")]
-        [SerializeField]
-        public FMODUnity.EventReference MusicEvent;
+        [Space(30)] [Header("Music")] [SerializeField]
+        public EventReference MusicEvent;
         public FMOD.Studio.EventInstance MusicInstance;
 
-        [Space(30)]
-        [Header("UI")]
-        [SerializeField]
-        public FMODUnity.EventReference UiOpenedEvent;
-       //public FMOD.Studio.EventInstance UiOpenedInstance;
+        [Space(30)] [Header("UI")] [SerializeField]
+        public EventReference UiOpenedEvent;
+        //public FMOD.Studio.EventInstance UiOpenedInstance;
 
-        public FMODUnity.EventReference ButtonClickedEvent;
+        public EventReference ButtonClickedEvent;
         //public FMOD.Studio.EventInstance ButtonClickedInstance;
 
-        public FMODUnity.EventReference OverButtonEvent;
+        public EventReference OverButtonEvent;
         //public FMOD.Studio.EventInstance OverButtonInstance;
 
+        private Transform _fmodInstance;
 
-
-         void Start()
+        private void Start()
         {
-            AmbInstance = FMODUnity.RuntimeManager.CreateInstance(AmbEvent);
+            AmbInstance = RuntimeManager.CreateInstance(AmbEvent);
             AmbInstance.start();
-           
+
+            var fmodObject = new GameObject("FmodInstance");
+            _fmodInstance = fmodObject.transform;
         }
 
         public void OnCityEntered()
         {
-            
-            
-        
-          
             Debug.Log("city_enter");
         }
 
         public void OnCityExited()
         {
-            
-            
         }
 
         public void OnPortEntered()
         {
-
             AmbInstance.setParameterByName("amb_condition", 1f);
         }
 
@@ -75,82 +63,80 @@ namespace Global.Services.Sounds.Runtime
 
         public void OnBattleEntered()
         {
-            
         }
 
         public void OnBattleExited()
         {
-            
         }
 
         public void OnCannonBallShot(Vector2 position)
         {
-            ShotInstance = FMODUnity.RuntimeManager.CreateInstance(ShotEvent);
-            //FMODUnity.RuntimeManager.AttachInstanceToGameObject(ShotInstance, Transform.po);
-            ShotInstance.setParameterByName("shot_type", 0f);
-            ShotInstance.start();
-            ShotInstance.release();
+            PlayShot(0f, position);
+            
             Debug.Log("boom");
         }
 
         public void OnShrapnelShot(Vector2 position)
         {
-            ShotInstance = FMODUnity.RuntimeManager.CreateInstance(ShotEvent);
-            FMODUnity.RuntimeManager.AttachInstanceToGameObject(ShotInstance, Transform.po);
-            ShotInstance.setParameterByName("shot_type", 1f);
-            ShotInstance.start();
-            ShotInstance.release();
+            PlayShot(1f, position);
+            
             Debug.Log("boom");
         }
 
         public void OnKnuppelShot(Vector2 position)
         {
-                        ShotInstance = FMODUnity.RuntimeManager.CreateInstance(ShotEvent);
-            //FMODUnity.RuntimeManager.AttachInstanceToGameObject(ShotInstance, Transform.po);
-            ShotInstance.setParameterByName("shot_type", 1f);
+            PlayShot(2f, position);
+            
+            Debug.Log("boom");
+        }
+
+        private void PlayShot(float parameter, Vector2 position)
+        {
+            ShotInstance = RuntimeManager.CreateInstance(ShotEvent);
+            AttachInstance(ShotInstance, position);
+            
+            ShotInstance.setParameterByName("shot_type", parameter);
             ShotInstance.start();
             ShotInstance.release();
-            Debug.Log("boom");
         }
 
         public void OnProjectileDropped(Vector2 position)
         {
-            
         }
 
         public void OnEnemyDamaged(Vector2 position)
         {
-            
         }
 
         public void OnDamageReceived()
         {
-            
         }
 
         public void OnUiOpened()
         {
-           FMODUnity.RuntimeManager.PlayOneShot(UiOpenedEvent); 
+            RuntimeManager.PlayOneShot(UiOpenedEvent);
         }
 
         public void OnOverButton()
         {
-            
         }
 
         public void OnButtonClicked()
         {
-            
         }
 
         public void OnMenuEntered()
         {
-            
         }
 
         public void OnMenuExited()
         {
-            
+        }
+
+        private void AttachInstance(EventInstance instance, Vector2 position)
+        {
+            _fmodInstance.position = position;
+            RuntimeManager.AttachInstanceToGameObject(instance, _fmodInstance);
         }
     }
 }

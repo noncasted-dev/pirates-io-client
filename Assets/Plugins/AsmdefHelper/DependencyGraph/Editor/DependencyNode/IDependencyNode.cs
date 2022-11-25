d1120644ed7430a6124441f62b93515f39d111b3
noncasted <noncasted@gmail.com>
2022-11-25 05:36:51 +0300

    wip

--- Assets/Plugins/AsmdefHelper/DependencyGraph/Editor/DependencyNode/IDependencyNode.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace AsmdefHelper.DependencyGraph.Editor.DependencyNode {
-    public interface IDependencyNode {
-        NodeProfile Profile { get; }
-        ICollection<NodeProfile> Sources  { get; }
-        ICollection<NodeProfile> Destinations { get; }
-    }
-}
--- Assets/Plugins/AsmdefHelper/DependencyGraph/Editor/IAsmdefNodeView.cs
+++
@@ -1,8 +0,0 @@
-using AsmdefHelper.DependencyGraph.Editor.NodeView;
-
-namespace AsmdefHelper.DependencyGraph.Editor {
-    public interface IAsmdefNodeView : INodeView {
-        IPort LeftPort { get; }
-        IPort RightPort { get; }
-    }
-}

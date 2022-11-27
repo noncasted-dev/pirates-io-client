7970d5bb4fd72107eaa2f861572d12863a7b611d
glebster51 <glebster51@rambler.ru>
2022-11-27 12:38:26 +0400

    снаряды

--- Assets/Features/Common/VFX/Floated.cs
+++ Assets/Features/Common/VFX/Floated.cs
@@ -9,14 +9,15 @@ public class Floated : MonoBehaviour
     [SerializeField] private List<Transform> _targets; 
     [SerializeField][Range(0f,1f)] private List<float> _floats;
     [SerializeField] private float _speed;
+    [SerializeField] private float _forceY = 0.1F;
     [SerializeField] private Vector2 _angles;
     private void OnEnable() => StartCoroutine(Floating());
     IEnumerator Floating()
     { 
         List<Vector3> startPoses = new List<Vector3>();
+        _floats = new List<float>();
         foreach (var target in _targets)
         {
-            _floats = new List<float>();
             startPoses.Add(target.position);
             _floats.Add(Random.value);
         }
@@ -27,7 +28,7 @@ public class Floated : MonoBehaviour
                 _floats[i] += Time.deltaTime * _speed;
                 float t = Mathf.PI * 2f * _floats[i];
                 float cos = Mathf.Cos(t);
-                _targets[i].position = startPoses[i] + Vector3.up * cos;
+                _targets[i].position = startPoses[i] + Vector3.up * cos * _forceY;
                 float angle = Mathf.LerpAngle(_angles.x, _angles.y, (cos + 1) * 0.5f);
                 _targets[i].rotation = Quaternion.Euler(0, 0, angle);
                 yield return null;
---
+++ Assets/Features/GamePlay/Services/Projectiles/Implementation/Linear/Knuppel/RandomSpriteAndRotation.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpriteAndRotation : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> sprites;
+    [SerializeField] private SpriteRenderer renderer;
+    
+    private void OnEnable()
+    {
+        
+    }
+}

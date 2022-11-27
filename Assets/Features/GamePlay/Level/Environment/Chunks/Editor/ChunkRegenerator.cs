d26dabbd11df9d1f75f702103578bcbe633b323a
noncasted <noncasted@gmail.com>
2022-11-27 16:36:03 +0300

    wip

---
+++ Assets/Features/GamePlay/Level/Environment/Chunks/Editor/ChunkRegenerator.cs
@@ -0,0 +1,22 @@
+﻿using GamePlay.Level.Environment.Tools;
+using UnityEditor;
+using UnityEngine;
+
+namespace GamePlay.Level.Environment.Chunks.Editor
+{
+    public static class ChunkRegenerator 
+    {
+        [MenuItem("Environment/RegenerateGround")]
+        public static void RegenerateGround()
+        {
+            var first = Object.FindObjectsOfType<FirstLayerBuilder>();
+            var second = Object.FindObjectsOfType<SecondLayerBuilder>();
+
+            foreach (var builder in first)
+                builder.Generate();
+            
+            foreach (var builder in second)
+                builder.Generate();
+        }
+    }
+}

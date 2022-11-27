4d5d3ff170b758e74aa57e7a6bb3f58590609800
glebster51 <glebster51@rambler.ru>
2022-11-27 12:43:37 +0400

    no message

--- Assets/Features/GamePlay/Services/Projectiles/Implementation/Linear/Knuppel/RandomSpriteAndRotation.cs
+++ Assets/Features/GamePlay/Services/Projectiles/Implementation/Linear/Knuppel/RandomSpriteAndRotation.cs
@@ -1,15 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RandomSpriteAndRotation : MonoBehaviour
 {
     [SerializeField] private List<Sprite> sprites;
-    [SerializeField] private SpriteRenderer renderer;
-    
+    [SerializeField] private SpriteRenderer renderer;    
     private void OnEnable()
     {
-        
+        renderer.transform.rotation = quaternion.Euler(0,0, Random.value * 360f);
+        renderer.sprite = sprites[(int) (Random.value * sprites.Count)];
     }
 }

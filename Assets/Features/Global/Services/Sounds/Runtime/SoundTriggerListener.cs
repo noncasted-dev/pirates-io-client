308c8ffb3c842e444db357d8ffd5cc87e23a4f3a
savenkoalex0 <84133206+savenkoalex0@users.noreply.github.com>
2022-11-29 02:44:26 +0300

    q

--- Assets/Features/Global/Services/Sounds/Runtime/SoundTriggerListener.cs
+++ Assets/Features/Global/Services/Sounds/Runtime/SoundTriggerListener.cs
@@ -31,6 +31,7 @@ namespace Global.Services.Sounds.Runtime
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             _healthListener?.Dispose();
 =======
 >>>>>>> main
@@ -48,6 +49,8 @@ namespace Global.Services.Sounds.Runtime
 =======
             _healthListener?.Dispose();
 >>>>>>> 9db0c19b09f4a28ade4e600cc9f3fed38807e4ab
+=======
+>>>>>>> main
         }
 
         private void OnSoundTriggered(SoundEvent data)
